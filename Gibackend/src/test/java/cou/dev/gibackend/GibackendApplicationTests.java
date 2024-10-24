e99a291abe2b38af963a654ca7583718d8f64488
sparkle6979 <liuhl79@163.com>
2024-10-24 21:08:37 +0800

    init project

---
+++ Gibackend/src/test/java/cou/dev/gibackend/GibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cou.dev.gibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
